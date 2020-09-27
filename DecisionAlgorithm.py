871963fcd11a1c1a7610d111933dc9620b1459e9
michellesakam <vanessasakam@gmail.com>
2020-09-27 16:45:16 +0200

    Implement decision tree algorithm

---
+++ DecisionAlgorithm.py
@@ -0,0 +1,67 @@
+from sklearn.tree import DecisionTreeRegressor
+import numpy as np
+
+
+def mse(y_pred, y_target):
+    return np.mean((y_pred - y_target) ** 2)
+
+
+def mae(y_pred, y_target):
+    return np.mean(abs(y_pred - y_target))
+
+
+class DecisionTreeRegressionTraining():
+
+    def __init__(self, criterion, max_tree_depth, rf_seed, input, target):
+        self.criterion = criterion
+        self.max_tree_depth = max_tree_depth
+        self.rf_seed = rf_seed
+
+        self.target_train = target[0]
+        self.target_cv = target[1]
+        self.x_train = input[0]
+        self.x_cv = input[1]
+
+    def __call__(self):
+        regressor = DecisionTreeRegressor(criterion=self.criterion,
+                                          max_depth=self.max_tree_depth, random_state=self.rf_seed)
+
+        regressor.fit(self.x_train, self.target_train)
+
+        # Predict the cv set
+        y_pred_train = regressor.predict(self.x_train)
+        y_pred_cv = regressor.predict(self.x_cv)
+
+        if self.criterion == "mse":
+            criterion_train = mse(y_pred_train, self.target_train)
+            criterion_cv = mse(y_pred_cv, self.target_cv)
+        elif self.criterion == "mae":
+            criterion_train = mae(y_pred_train, self.target_train)
+            criterion_cv = mae(y_pred_cv, self.target_cv)
+
+        print(self.criterion + " train =", criterion_train)
+        print(self.criterion + " cv =", criterion_cv)
+
+        return regressor, y_pred_train, y_pred_cv
+
+
+class  DecisionTreeRegressionTesting():
+
+    def __init__(self, model, criterion, input, target):
+        self.model = model
+        self.criterion = criterion
+        self.target_test = target[2]
+        self.x_test = input[2]
+
+    def __call__(self):
+        # Predict the test set
+        y_pred_test = self.model.predict(self.x_test)
+
+        if self.criterion == "mse":
+            criterion_test = mse(y_pred_test, self.target_test)
+        elif self.criterion == "mae":
+            criterion_test = mae(y_pred_test, self.target_test)
+
+        print(self.criterion + " test =", criterion_test)
+
+        return y_pred_test
--- config_file.py
+++ config_file.py
@@ -1,13 +1,14 @@
 from HousePricesDatabase import HousePricesDatabase
 from DataPreprocessing import DataPreprocessing
 from RFAlgorithm import RandomForestTraining, RandomForestTesting
+from DecisionAlgorithm import DecisionTreeRegressionTraining, DecisionTreeRegressionTesting
 from Analysis import Analysis
 
 import numpy as np
 # ======================================================================================================================
 """ All the parameters that can be chosen
 # Nominal parameters:
-nominal = ["MS SubClass", "MS Zoning", "Street", "Alley", "Land Contour", "Lot Config", "Neighborhood", 
+nominal = ["MS SubClass", "MS Zoning", "Street", "Alley", "Land Contour", "Lot Config", "Neighborhood",
            "Condition 1", "Condition 2", "Bldg Type", "House Style", "Roof Style", "Roof Matl", "Exterior 1st",
            "Exterior 2nd", "Mas Vnr Type", "Foundation", "Heating", "Central Air", "Garage Type", "Misc Feature",
            "Sale Type", "Sale Condition"]
@@ -21,12 +22,12 @@ continuous = ["Lot Frontage", "Lot Area", "Mas Vnr Area", "BsmtFin SF 1", "BsmtF
 # Ordinal parameters:
 ordinal = ["Lot Shape", "Utilities", "Land Slope", "Overall Qual", "Overall Cond", "Exter Qual", "Exter Cond",
            "Bsmt Qual", "Bsmt Cond", "Bsmt Exposure", "BsmtFin Type 1", "BsmtFin Type 2", "Heating QC", "Electrical",
-           "Kitchen Qual", "Functional", "Fireplace Qu", "Garage Finish", "Garage Qual", "Garage Cond", 
+           "Kitchen Qual", "Functional", "Fireplace Qu", "Garage Finish", "Garage Qual", "Garage Cond",
            "Paved Drive", "Pool QC", "Fence"]
 
 # Discrete parameters:
-discrete = ["Year Built", "Year Remod/Add", "Bsmt Full Bath", "Bsmt Half Bath", "Full Bath", "Half Bath", 
-            "Bedroom AbvGr", "Kitchen AbvGr", "TotRms AbvGrd", "Fireplaces", "Garage Yr Blt", "Garage Cars", 
+discrete = ["Year Built", "Year Remod/Add", "Bsmt Full Bath", "Bsmt Half Bath", "Full Bath", "Half Bath",
+            "Bedroom AbvGr", "Kitchen AbvGr", "TotRms AbvGrd", "Fireplaces", "Garage Yr Blt", "Garage Cars",
             "Mo Sold", "Yr Sold"]
 """
 
@@ -63,8 +64,8 @@ protocol = [0.8, 0.1, 0.1]
 n_trees = 50
 criterion = "mse"
 max_tree_depth = 10
-rf_seed = 10  # Used to fix the random_state of RF regressor to ensure reproducibility
-
+rf_seed = 10  # Used to fix the random_state of RF and decision tree regressors to ensure reproducibility
+flag = "" # to distinguish random forest to decision tree algorithm
 # ======================================================================================================================
 # Toolchain
 print("1. Load Database")
@@ -81,19 +82,26 @@ print("-----> RF")
 rf_train = RandomForestTraining(n_trees, criterion, rf_seed, max_tree_depth, X, y)
 regressor, y_predict_train, y_predict_cv = rf_train()
 
-print("-----> Decision Tree")
+print("4.-----> Decision Tree")
 # Put here your code :)
+decision_train = DecisionTreeRegressionTraining(criterion, rf_seed, max_tree_depth, X, y)
+d_regressor, d_y_predict_train, d_y_predict_cv = decision_train()
 
-print("4. RF test")
+print("5. RF test")
 print("-----> RF")
 regressor_testing = RandomForestTesting(regressor, criterion, X, y)
 y_predict_test = regressor_testing()
 
-print("-----> Decision Tree")
-# Put here your code :)
+print("6.-----> Decision Tree test")
+decision_regressor_testing = DecisionTreeRegressionTesting(d_regressor, criterion, X, y)
+d_y_predict_test = decision_regressor_testing()
 
 # 5. Analysis
-print("5. Analysis")
-score = Analysis((y_predict_train, y_predict_cv, y_predict_test), y, mean_sale_price, std_sale_price)
+print("7. Analysis with Random Forest")
+score = Analysis((y_predict_train, y_predict_cv, y_predict_test), y, mean_sale_price, std_sale_price, flag = "RF")
+score()
+
+print("8. Analysis with Decision tree")
+score = Analysis((d_y_predict_train, d_y_predict_cv, d_y_predict_test), y, mean_sale_price, std_sale_price, flag = "DT")
 score()
 # To adapt
