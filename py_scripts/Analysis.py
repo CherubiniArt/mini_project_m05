cddd955eb91ebe83b98e90248d08ec984fa41e28
Arthur <cherubini.art@gmail.com>
2020-09-28 17:44:18 +0200

    Final structure of the code

--- py_scripts/Analysis.py
+++ py_scripts/Analysis.py
@@ -1,13 +1,14 @@
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Analysis():
 
-    def __init__(self, y_pred_rf, target, mean_target, std_target, flag):
-        self.y_pred_rf_train =y_pred_rf[0]
-        self.y_pred_rf_cv = y_pred_rf[1]
-        self.y_pred_rf_test = y_pred_rf[2]
+    def __init__(self, y_pred_train, y_pred_cv, y_pred_test, target, mean_target, std_target, algorithm):
+        self.y_pred_train = y_pred_train
+        self.y_pred_cv = y_pred_cv
+        self.y_pred_test = y_pred_test
 
         self.y_train = target[0]
         self.y_cv = target[1]
@@ -16,20 +17,50 @@ class Analysis():
         self.mean = mean_target
         self.std = std_target
 
-        self.flag = flag
+        self.algorithm = algorithm
+
+    def autolabel(self, ax, rects):
+        for rect in rects:
+            h = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width() / 2., 1.0 * h, '%f' % h,
+                    ha='center', va='bottom')
+
+    def plot_multiple_bars(self, scores_train, scores_cv, scores_test):
+        N = len(self.algorithm)
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.27  # the width of the bars
+
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        rects1 = ax.bar(ind, scores_train, width, color='r')
+        rects2 = ax.bar(ind + width, scores_cv, width, color='g')
+        rects3 = ax.bar(ind + width * 2, scores_test, width, color='b')
+
+        ax.set_ylabel('R2 Scores')
+        ax.set_xticks(ind + width)
+        ax.set_xticklabels(self.algorithm)
+        ax.legend((rects1[0], rects2[0], rects3[0]), ('train', 'cv', 'test'))
+
+        self.autolabel(ax, rects1)
+        self.autolabel(ax, rects2)
+        self.autolabel(ax, rects3)
+
+        plt.show()
 
     def __call__(self):
-        # Compute r2:
-        r2_train = r2_score(self.y_train, self.y_pred_rf_train)
-        r2_cv = r2_score(self.y_cv, self.y_pred_rf_cv)
-        r2_test = r2_score(self.y_test, self.y_pred_rf_test)
+        r2_train = []
+        r2_cv = []
+        r2_test = []
 
-        print("r2 train =", r2_train)
-        print("r2 cv =", r2_cv)
-        print("r2 test =", r2_test)
+        for i, alg in enumerate(self.algorithm):
+            r2_train.append(r2_score(self.y_train, self.y_pred_train[i]))
+            r2_cv.append(r2_score(self.y_cv, self.y_pred_cv[i]))
+            r2_test.append(r2_score(self.y_test, self.y_pred_test[i]))
 
-        plt.bar(["train", "cv", "test"], [r2_train, r2_cv, r2_test])
-        plt.ylabel("r2")
-        title = " Random Forest" if self.flag=="RF" else " Decision Tree Regression"
-        plt.title("Performance of the" + title  + " algorithm")
-        plt.show()
+            print(alg+":")
+            print("r2 train =", r2_train[-1])
+            print("r2 cv =", r2_cv[-1])
+            print("r2 test =", r2_test[-1])
+
+        self.plot_multiple_bars(r2_train, r2_cv, r2_test)
--- py_scripts/DecisionAlgorithm.py
+++ py_scripts/DecisionAlgorithm.py
@@ -1,21 +1,13 @@
 from sklearn.tree import DecisionTreeRegressor
-import numpy as np
-
-
-def mse(y_pred, y_target):
-    return np.mean((y_pred - y_target) ** 2)
-
-
-def mae(y_pred, y_target):
-    return np.mean(abs(y_pred - y_target))
+from py_scripts.utils import regressor_training
 
 
 class DecisionTreeRegressionTraining():
 
-    def __init__(self, criterion, max_tree_depth, rf_seed, input, target):
+    def __init__(self, criterion, max_tree_depth, seed, input, target):
         self.criterion = criterion
         self.max_tree_depth = max_tree_depth
-        self.rf_seed = rf_seed
+        self.seed = seed
 
         self.target_train = target[0]
         self.target_cv = target[1]
@@ -23,45 +15,7 @@ class DecisionTreeRegressionTraining():
         self.x_cv = input[1]
 
     def __call__(self):
-        regressor = DecisionTreeRegressor(criterion=self.criterion,
-                                          max_depth=self.max_tree_depth, random_state=self.rf_seed)
+        regressor = DecisionTreeRegressor(criterion=self.criterion, max_depth=self.max_tree_depth,
+                                          random_state=self.seed)
 
-        regressor.fit(self.x_train, self.target_train)
-
-        # Predict the cv set
-        y_pred_train = regressor.predict(self.x_train)
-        y_pred_cv = regressor.predict(self.x_cv)
-
-        if self.criterion == "mse":
-            criterion_train = mse(y_pred_train, self.target_train)
-            criterion_cv = mse(y_pred_cv, self.target_cv)
-        elif self.criterion == "mae":
-            criterion_train = mae(y_pred_train, self.target_train)
-            criterion_cv = mae(y_pred_cv, self.target_cv)
-
-        print(self.criterion + " train =", criterion_train)
-        print(self.criterion + " cv =", criterion_cv)
-
-        return regressor, y_pred_train, y_pred_cv
-
-
-class  DecisionTreeRegressionTesting():
-
-    def __init__(self, model, criterion, input, target):
-        self.model = model
-        self.criterion = criterion
-        self.target_test = target[2]
-        self.x_test = input[2]
-
-    def __call__(self):
-        # Predict the test set
-        y_pred_test = self.model.predict(self.x_test)
-
-        if self.criterion == "mse":
-            criterion_test = mse(y_pred_test, self.target_test)
-        elif self.criterion == "mae":
-            criterion_test = mae(y_pred_test, self.target_test)
-
-        print(self.criterion + " test =", criterion_test)
-
-        return y_pred_test
+        return regressor_training(regressor, self.x_train, self.x_cv, self.target_train, self.target_cv, self.criterion)
