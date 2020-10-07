bbaf47922e6fc4f86c382d78b528ffae95231278
Arthur <cherubini.art@gmail.com>
2020-10-07 15:06:10 +0200

    Fix database import

--- house_prices_m05_pkg/__init__.py
+++
@@ -1,7 +0,0 @@
-#from .py_scripts.run_toolchain import *
-#from .py_scripts.utils import *
-#from .py_scripts.Analysis import *
-#from .py_scripts.DataPreprocessing import *
-#from .py_scripts.DecisionAlgorithm import *
-#from .py_scripts.HousePricesDatabase import *
-#from .py_scripts.RFAlgorithm import *
--- house_prices_m05_pkg/py_scripts/__init__.py
+++
@@ -1,7 +0,0 @@
-#from .Analysis import Analysis
-#from .DataPreprocessing import DataPreprocessing
-#from .DecisionAlgorithm import DecisionTreeRegressionTraining
-#from .HousePricesDatabase import HousePricesDatabase
-#from .RFAlgorithm import RandomForestTraining
-#from .run_toolchain import run_toolchain
-#from .utils import *
