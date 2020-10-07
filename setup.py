14da2b31888e31637eedb6f79abb9672bb1dc7dc
Michelle Vanessa Meguep Sakam <M287931@eu.merckgroup.com>
2020-10-07 12:27:51 +0200

    add setup file to the project

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+
+def load_requirements(f):
+    retval = [str(k.strip()) for k in open(f, "rt")]
+    return [k for k in retval if k and k[0] not in ("#", "-")]
+
+
+setup(
+    name="house_prices_m05_pkg",
+    version="1.0.0",
+    description="house prices prediction tool",
+    url="https://github.com/CherubiniArt/mini_project_m05",
+    license="BSD",
+    author="Arthur Cherubini & Michelle Meguep",
+    author_email="cherubini.art@gmail.com",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=load_requirements("requirements.txt"),
+    entry_points={"console_scripts": ["run-house-prices-pred = house_prices_m05_pkg.toolchain_all_params"]},
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
