6f6f040b498611ad49460d811259dfa612b08187
Michelle Vanessa Meguep Sakam <M287931@eu.merckgroup.com>
2020-10-07 12:57:52 +0200

    update of the init in pyscript

---
+++ house_prices_m05_pkg/py_scripts/__init__.py
@@ -0,0 +1,5 @@
+from .Analysis import Analysis
+from .DataPreprocessing import DataPreprocessing
+from .DecisionAlgorithm import DecisionTreeRegressionTraining
+from .HousePricesDatabase import HousePricesDatabase
+from .RFAlgorithm import RandomForestTraining
