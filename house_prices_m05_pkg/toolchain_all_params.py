6d2470c84c548034f9817a5ecc8dc2bea019c9bf
Arthur <cherubini.art@gmail.com>
2020-10-07 15:22:07 +0200

    pkg resources to import db file

--- house_prices_m05_pkg/toolchain_all_params.py
+++ house_prices_m05_pkg/toolchain_all_params.py
@@ -22,7 +22,7 @@ def main():
 
     # Path to the db used for the regression task
 
-    db_path = "./house-prices/house-prices.csv"
+    db_path = "house-prices.csv"
 
     # Protocol used to split the dataset into train/cv/test
     protocol = [0.8, 0.1, 0.1]
